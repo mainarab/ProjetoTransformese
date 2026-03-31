db445dc686838e874fc9d5a6cfe2299b770123d9
Mainara Barbosa <206761415+mainarab@users.noreply.github.com>
2026-03-31 17:48:12 -0300

    Fix: animação de abrir e fechar menu mobile

--- components/header/header.js
+++ components/header/header.js
@@ -6,36 +6,50 @@ const logo = document.getElementsByClassName("logo");
 const containerMobile = document.querySelector(".containerMobile");
 const menuClose = document.getElementById("menuClose");
 
-  menuHamburguer.addEventListener("click", (e) => {
-    e.stopPropagation(); 
-    containerMobile.classList.add("ativo");
-  });
+menuHamburguer.addEventListener("click", (e) => {
+  e.stopPropagation();
+  containerMobile.classList.add("ativo");
+  gsap.fromTo(
+    containerMobile,
+    { x: "-100%" },
+    { x: "0%", duration: 0.5, ease: "power3.out" },
+  );
+});
 
-  menuClose.addEventListener("click", () => {
-    containerMobile.classList.remove("ativo");
-    document.body.style.overflow = "auto";
-  });
-
-
-  document.addEventListener("click", (e) => {
-    if (
-      containerMobile.classList.contains("ativo") &&
-      !containerMobile.contains(e.target) &&
-      e.target !== menuHamburguer
-    ) {
+menuClose.addEventListener("click", () => {
+  gsap.to(containerMobile, {
+    x: "-100%",
+    duration: 0.5,
+    ease: "power3.in",
+    onComplete: () => {
       containerMobile.classList.remove("ativo");
-      document.body.style.overflow = "auto";
-    }
+    },
   });
+});
 
-
+document.addEventListener("click", (e) => {
+  gsap.to(containerMobile, {
+    x: "-100%",
+    duration: 0.5,
+    ease: "power3.in",
+    onComplete: () => {
+      if (
+        containerMobile.classList.contains("ativo") &&
+        !containerMobile.contains(e.target) &&
+        e.target !== menuHamburguer
+      ) {
+        containerMobile.classList.remove("ativo");
+        document.body.style.overflow = "auto";
+      }
+    },
+  });
+});
 
 const trocarTema = document.querySelectorAll(".toggleDark");
 
-trocarTema.forEach(botao => {
+trocarTema.forEach((botao) => {
   botao.addEventListener("click", (e) => {
     e.stopPropagation();
     document.body.classList.toggle("dark");
   });
 });
-
