4b24c45c9bfea027398aa1fbda3a1122ed1bb4ea
Mainara Barbosa <206761415+mainarab@users.noreply.github.com>
2026-03-31 19:48:20 -0300

    Fix: Ajuste de caminhos do header

--- js/script.js
+++ js/script.js
@@ -16,12 +16,3 @@ for (let i = 0; i < elements.length; i++) {
   observer.observe(elements[i]);
 }
 
-document.querySelector("#toggle-dark").addEventListener("click", (e) => {
-  e.stopPropagation(); 
-  document.body.classList.toggle("dark");
-});
-
-document.querySelector(".menu-toggle").addEventListener("click", (e) => {
-  e.stopPropagation();
-  document.querySelector(".menu").classList.toggle("active");
-});
