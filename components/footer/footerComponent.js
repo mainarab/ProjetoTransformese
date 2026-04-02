1251e2c14de4110c227ab65ce6860ef4b06eaf94
Mainara Barbosa <206761415+mainarab@users.noreply.github.com>
2026-04-01 21:33:45 -0300

    FIX: componentização do footer

---
+++ components/footer/footerComponent.js
@@ -0,0 +1,58 @@
+export default class Footer {
+  constructor({ container }) {
+    this.container = container;
+    this.render();
+  }
+
+  render() {
+    this.container.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="cm-footer">
+        <footer class="footer">
+
+          <div class="footer-contact"> 
+            <h3>Contato</h3> 
+
+            <div class="contato-lado">   
+              <div class="contato-link">
+                <i class="fa-solid fa-phone iconFooter"></i>
+                <p>(11) 9999-9999</p>
+              </div>
+
+              <div class="contato-link">
+                <i class="fa-solid fa-envelope iconFooter"></i>
+                <p>contato@controlmoney.com.br</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="redes">
+            <h2 class="h2Redes">Redes Sociais</h2>
+            <nav aria-label="Redes Sociais">
+              <ul>
+                <li><a href="#"><img class="insta" src="" alt="Instagram"></a></li>
+                <li><a href="#"><img class="facebook" src="" alt="Facebook"></a></li>
+                <li><a href="#"><img class="x" src="" alt=""></a></li>
+                <li><a href="#"><img class="youtube" src="" alt="Youtube"></a></li>
+              </ul>
+            </nav>
+          </div>
+
+          <div class="outros">
+            <nav>
+              <ul>
+                <li><a href="/ProjetoTransformese/pages/login/page.html">Controle de gastos</a></li>
+                <li><a href="/ProjetoTransformese/pages/sobre-nos/page.html">Sobre nós</a></li>
+                <li><a href="#">Missão</a></li>
+                <li><a href="pages/sac/page.html">SAC</a></li>
+              </ul>
+            </nav>
+          </div>
+
+        </footer>
+      </div>
+      `
+    );
+  }
+}
