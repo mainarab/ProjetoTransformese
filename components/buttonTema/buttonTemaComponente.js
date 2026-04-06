0ef5388a4011d6064a37097080632613ef531eaf
Mainara Barbosa <206761415+mainarab@users.noreply.github.com>
2026-04-06 16:02:09 -0300

    Fix: ajuste no componente do botão que troca de tema, adicionado na página de login

--- components/buttonTema/buttonTemaComponente.js
+++ components/buttonTema/buttonTemaComponente.js
@@ -1,19 +1,20 @@
 export default class ButtonTema {
   constructor({ container }) {
     this.container = container;
-
     this.render();
     this.cacheElements();
     this.addEvents();
+
+    this.updateIcon();
   }
 
   render() {
     this.container.insertAdjacentHTML(
-      "beforeend",
+      "afterbegin",
       `
       <div class="btnTema">
         <div class="iconTema">
-          <img class="iconImg" src="../../img/moon.svg" alt="tema">
+          <i class="fa-solid fa-moon iconBtnTema"></i>
         </div>
       </div>
       `
@@ -22,8 +23,8 @@ export default class ButtonTema {
 
   cacheElements() {
     this.btn = this.container.querySelector(".btnTema");
-    this.icon = this.container.querySelector(".iconTema");
-    this.img = this.container.querySelector(".iconImg");
+this.btn.offsetHeight; // força o reflow
+    this.icon = this.container.querySelector(".iconBtnTema");
   }
 
   addEvents() {
@@ -36,10 +37,37 @@ export default class ButtonTema {
     this.btn.classList.toggle("btnTemaDark");
     document.body.classList.toggle("dark");
 
+    // Troca o ícone
     if (this.btn.classList.contains("btnTemaDark")) {
-      this.img.src = "../../img/sun.svg";
+      this.icon.classList.remove("fa-moon");
+      this.icon.classList.add("fa-sun");
     } else {
-      this.img.src = "../../img/moon.svg";
+      this.icon.classList.remove("fa-sun");
+      this.icon.classList.add("fa-moon");
+    }
+  }
+
+  updateIcon() {
+    // Ajusta o ícone ao carregar, caso o body já esteja em dark mode
+    if (document.body.classList.contains("dark")) {
+      this.icon.classList.remove("fa-moon");
+      this.icon.classList.add("fa-sun");
+      this.btn.classList.add("btnTemaDark");
+    } else {
+      this.icon.classList.remove("fa-sun");
+      this.icon.classList.add("fa-moon");
+      this.btn.classList.remove("btnTemaDark");
     }
   }
 }
+
+//Precisa adicionar esses links para ele funcionar, o import é no css da página
+// <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"/>
+// @import url(../buttonTema/buttonTema.css);
+
+//no js coloque:
+//import ButtonTema from "../../components/buttonTema/buttonTemaComponente.js"; new ButtonTema({ container: document.querySelector(".btnTemaComp") }); </script>
+
+//e adicione no lugar onde quer que o botão apareça:
+// <div class="btnTemaComp"></div>
+
